d272208c310b325f79e5bce9f4a6deb14dc6e152
Pruthvi Dasari <pruthvid274@gmail.com>
2026-06-06 16:15:03 +0530

    Add ThreadDemo class with multithreading example

---
+++ 01-Introduction/Day-4/JavaPrograms/ThreadDemo.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread {
+
+    public void run() {
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Thread Running : " + i);
+
+            try {
+                Thread.sleep(500);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.start();
+        t2.start();
+    }
+}
